351dd169d5589c55b969e63e2422bfa66a2a4997
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-15 22:23:01 +0100

    added comments

--- src/GenericArrayWrapper.java
+++ src/GenericArrayWrapper.java
@@ -8,29 +8,28 @@ public class GenericArrayWrapper <T>{
     private T[] array;
     private Integer elementCount = 0;
 
-    // constructor methode mit Angabe der Anzahl der Elemente (Länge des Arrays)
+    // constructor methode mit Angabe der Anzahl der Elemente (Größe des Arrays)
     // constructor method used to initialize objects. It is called when an object of a class is created.
-    // bei List "size" statt "length"
     public GenericArrayWrapper(int size) {
-        this.array = (T[]) new Object[size]; // beim Erstellen des Arrays soll die Länge angegeben werden
+
+        this.array = (T[]) new Object[size]; // beim Erstellen des Arrays soll die Größe angegeben werden
         this.elementCount = 0;
-        // "int[] array = new int[length]" : Erstellen des Arrays
+        // "T[] array = (T[]) new Object[size]" : Bei der Objekterstellung wird ein Objekt anstelle des Platzhalters eingefügt.
     }
 
     // add-Methode; Elemente hinzufügen
     public void addToArray(T newElement) {
+
         if (elementCount < array.length) {
             array[elementCount++] = newElement;
-        } else {                                        // wenn Länge nicht ausreicht, soll Länge verdoppelt werden
-            T[] newArray = (T[]) new Object[this.elementCount*2]; // neues Array erstellen mit doppelter Länge
+        } else {                                      // wenn Länge nicht ausreicht, soll Länge verdoppelt werden
+            T[] newArray = (T[]) new Object[this.elementCount*2]; // neues Objekt erstellen anstelle des Platzhalters (T) mit doppelter Länge
 
             System.arraycopy(array, 0, newArray, 0, array.length);
             newArray[elementCount] = newElement;     // das neue Element in das neue Array hinzufügen
             elementCount++;
-            this.array = newArray;                          // das erste Array mit den Daten des neuen Arrays überschreiben
+            this.array = newArray;                  // das erste Array mit den Daten des neuen Arrays überschreiben
         }
-
-        // array = Arrays.copyOf(array, elementCount*2); macht copy von ersten Array in ein neues Array mit neue Länge
     }
 
     // get-Methode; liefert das Element an bestimmten Stelle (index: Stelle)
@@ -39,17 +38,18 @@ public class GenericArrayWrapper <T>{
         if (index < this.array.length) {
             return this.array[index];
         }
-
         return null;
     }
 
-    // getArraySize-Methode; Länge des Arrays zurückliefern
+    // getArraySize-Methode; Größe des Arrays zurückliefern
     public int getArraySize() {
+
         return this.array.length;
     }
 
     // getElementCount-Methode; Anzahl der Elemente zurückliefert
     public int getElementCount() {
+
         return elementCount;
     }
 
